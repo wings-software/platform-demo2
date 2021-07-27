71b6e687b7dee5fcb4858a304c8c9a39bafa2c07
nickdurkin <nick.durkin@harness.io>
2021-07-27 08:10:00 -0700

    Update auth-module.php

--- nginx/src/auth-module.php
+++ nginx/src/auth-module.php
@@ -1,7 +1,7 @@
 <?php
 //Working on different version
 $auth_version=1; // app Version (1 or 2)
-$auth_icon="";
-$auth_message=""; // Landing page
+$auth_icon="https://yt3.ggpht.com/-qPeLEQ_cM4c/AAAAAAAAAAI/AAAAAAAAAAA/6R8IgGyYLlA/s900-c-k-no-mo-rj-c0xffffff/photo.jpg";
+$auth_message="hi"; // Landing page
 $my_link=""; // your link
 ?>
