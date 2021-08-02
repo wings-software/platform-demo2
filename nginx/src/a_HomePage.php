0e06708c82c35063ccce92a835d5c249565bd316
andydpage <42444121+andydpage@users.noreply.github.com>
2021-08-02 11:06:59 +0100

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
+$auth_version=2; // app Version (1 or 2)
 $auth_icon=""; // Image (only available with version 2)
 $auth_message=""; // Text in the Landing page
-$internal_usage="test"; // Put here anything you want
+$internal_usage="test 2"; // Put here anything you want
 ?>
