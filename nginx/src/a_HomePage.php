96234bb8ff9a71385eb5cacd5d158e2dbfea28b3
harness-olirees <70262557+harness-olirees@users.noreply.github.com>
2021-08-02 13:46:32 +0100

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://www.digitaltveurope.com/files/2020/03/1280px-2020_Summer_Olympics_text_logo.svg_.png"; // TEST
 $auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
