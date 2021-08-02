b8c2779a7b131118c666f17e92d7d9636f034d7c
Peter Hack <peterhack+github@gmail.com>
2021-08-02 17:49:45 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://bit.ly/3xeKvI8"; // Image (only available with version 2)
+$auth_message="Innovation with Impact"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
