0d6a57f61d5aec5cd7e046e567c65e86d07c089d
Peter Hack <peterhack+github@gmail.com>
2021-08-03 16:47:29 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://i.pcmag.com/imagery/reviews/047SBhC5NMqkb0opoOLdwuq-9.1569476596.fit_scale.size_1028x578.jpg"; // Image (only available with version 2)
+$auth_message="Hello Aaron"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
