8ec9860426b921bbb905c7c00d77f01cadf5d595
billy-harness <87822493+billy-harness@users.noreply.github.com>
2021-08-06 09:08:07 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://www.google.com/imgres?imgurl=https%3A%2F%2Ffederalnewsnetwork.com%2Fwp-content%2Fuploads%2F2021%2F05%2Fthundercat-logo.png&imgrefurl=https%3A%2F%2Ffederalnewsnetwork.com%2Fcme-event%2Fhow-to-create-an-ai-ready-infrastructure-for-your-agency%2F&tbnid=2__bkh4D6DH3XM&vet=12ahUKEwjB_bSwupzyAhX5wikDHVKYAWQQMygBegQIARA1..i&docid=w50suAUMUjO5GM&w=500&h=203&q=Thundercat%20federal&ved=2ahUKEwjB_bSwupzyAhX5wikDHVKYAWQQMygBegQIARA1"; // Image (only available with version 2)
+$auth_message="Hello Thundercat Team"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
