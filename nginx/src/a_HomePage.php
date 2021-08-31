584786ff3a77c70e8543cb31890aeed04b4f6743
Peter Hack <peterhack+github@gmail.com>
2021-08-31 15:15:43 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Trace3_logo.png/220px-Trace3_logo.png"; // Image (only available with version 2)
+$auth_message="Hi Derek!"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
