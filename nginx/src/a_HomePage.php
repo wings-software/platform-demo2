2779a7fc4c745568d455b0587a9b713b7a7d9425
damian-curiel <89002083+damian-curiel@users.noreply.github.com>
2021-09-03 10:48:49 -0500

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://s3.amazonaws.com/media.mixrank.com/hero-img/60047fa5128e9ef6678ccf4fea598377"; // Image (only available with version 2)
+$auth_message="Demo Harness para IXPAN"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
