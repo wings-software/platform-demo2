ee5164c28ab8bfe2ef0c5efdfd7581bc663f82d0
Luke Hertert <luke.hertert@gmail.com>
2021-09-08 19:29:09 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -4,7 +4,7 @@
 ///////////////////////
 
 $auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_icon="https://1000logos.net/wp-content/uploads/2017/06/United-logo.jpg"; // Image (only available with version 2)
+$auth_message="Gibsons"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
