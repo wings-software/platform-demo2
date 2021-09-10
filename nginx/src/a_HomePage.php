8aea615a4f8be7f1e38d31f4dce9c5d7b4f34cf0
gnogueira7 <74684800+gnogueira7@users.noreply.github.com>
2021-09-10 09:29:43 -0300

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,7 +3,7 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
+$auth_version=2; // app Version (1 or 2)
 $auth_icon=""; // Image (only available with version 2)
 $auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
