32ad312fb7dd9b68c909276b6520ba87f35b4c30
harness-olirees <70262557+harness-olirees@users.noreply.github.com>
2021-09-20 08:22:52 +0100

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -4,7 +4,7 @@
 ///////////////////////
 
 $auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
+$auth_icon="https://thumbs.dreamstime.com/z/space-ship-vector-icon-rocket-illustration-symbol-rocketship-logo-space-ship-vector-icon-rocket-illustration-symbol-rocketship-156155610.jpg"; // Image (only available with version 2)
 $auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
