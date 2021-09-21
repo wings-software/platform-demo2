25783459f764509297c866df0b1e46ce7bd29a72
Alex Valentine <alex.valentine@harness.io>
2021-09-21 14:33:02 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://cdn.mos.cms.futurecdn.net/a2qJThwZXEYsUm5ZauoqWW-970-80.jpg.webp"; // Image (only available with version 2)
+$auth_message="Space Force"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
