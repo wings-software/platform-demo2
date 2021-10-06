d4a3b72e0520974d3ae9f153f202621196e783c9
billy-harness <87822493+billy-harness@users.noreply.github.com>
2021-10-06 13:23:19 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://www.vcinity.io/sites/default/files/partners/meadowgate.png"; // Image (only available with version 2)
+$auth_message="Hello Meadowgate Team!"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
