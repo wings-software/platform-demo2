6c8e86c5d8139b949f399a2286bcf77a2367664a
gnogueira7 <74684800+gnogueira7@users.noreply.github.com>
2021-10-13 15:53:22 -0300

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -4,7 +4,7 @@
 ///////////////////////
 
 $auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
+$auth_icon="https://mma.prnewswire.com/media/779232/Harnes_logo_horizontal.jpg?p=facebook"; // Image (only available with version 2)
 $auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
