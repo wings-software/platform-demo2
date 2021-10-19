2902faeeeefed9d0ded0b21e6bc13b5a084e1f44
Alex Valentine <alex.valentine@harness.io>
2021-10-19 14:53:42 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://images.squarespace-cdn.com/content/v1/5c8686aaf8135a111aa0c741/1556128398551-O5MRIJ0FFZ7JNBCYDF3J/Three+Wire+Systems-01.png?format=1500w"; // Image (only available with version 2)
+$auth_message="Hi ThreeWire!"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
