0143b8b7ca10747d1f12d442560875351f1e9fa8
Luis Redda <luis.redda@harness.io>
2021-10-19 21:40:40 -0300

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
-$internal_usage=""; // Put here anything you want
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://harness.io/wp-content/uploads/2021/04/canary-standing-350x400-1.png"; // Image (only available with version 2)
+$auth_message="Harness Demo"; // Text in the Landing page
+$internal_usage="123"; // Put here anything you want
 ?>
