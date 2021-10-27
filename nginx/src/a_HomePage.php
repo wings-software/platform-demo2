a8eb52c115be300023f6f2e56b4df618d03fd06c
MarcoCelon <mcelon@gmail.com>
2021-10-27 13:27:14 +1100

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://sla-digital.com/content/uploads/2018/03/Untitled-design-24-1024x616.png"; // Image (only available with version 2)
+$auth_message="WelcomE to VuClip"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
