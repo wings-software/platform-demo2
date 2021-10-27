f17249785d8b4a4c4f98469f21cbac3c7299b745
MarcoCelon <mcelon@gmail.com>
2021-10-27 13:21:10 +1100

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vuclip.com%2F&psig=AOvVaw0fxfCZjaCibFYx1N-w_YWR&ust=1635387405153000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDI5P3C6fMCFQAAAAAdAAAAABAM"; // Image (only available with version 2)
 $auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
