038bf5b1f67f7a6e9cfcf7bd3392f05a8887dd45
harness-olirees <70262557+harness-olirees@users.noreply.github.com>
2021-11-02 09:41:29 +0000

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -4,7 +4,7 @@
 ///////////////////////
 
 $auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_icon="harness-olirees:patch-10"; // Image (only available with version 2)
+$auth_message="https://cio-institute.com/wp-content/uploads/2021/06/Global_CIO_Institute.png"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
