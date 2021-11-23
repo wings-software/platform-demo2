f8da5e2908b47d56534dbc051aac0111df2437bd
gabrielbhhprivate <94842498+gabrielbhhprivate@users.noreply.github.com>
2021-11-23 13:59:43 +0200

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -4,7 +4,7 @@
 ///////////////////////
 
 $auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
+$auth_icon="https://cdn.dribbble.com/users/946209/screenshots/2993623/neo.png"; // Image (only available with version 2)
 $auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
