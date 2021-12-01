1a62e0bbf1d30fb9106a36961daabf7ad4f54fd0
billy-harness <87822493+billy-harness@users.noreply.github.com>
2021-12-01 16:12:07 -0500

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://i.pinimg.com/736x/33/32/6d/33326dcddbf15c56d631e374b62338dc.jpg"; // Image (only available with version 2)
+$auth_message="I'm a really cute cat!"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
