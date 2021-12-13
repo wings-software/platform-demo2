93f60d9926ea18c31f78e55585c8077b87725393
damian-curiel <89002083+damian-curiel@users.noreply.github.com>
2021-12-13 17:02:58 -0600

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://cdn0.talenteca.com/company-images/TK_COMPANY_LOGO-2020_02_27_09_43_03-73605772677428460338.png"; // Image (only available with version 2)
+$auth_message="TelePerformance con Harness!!"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
