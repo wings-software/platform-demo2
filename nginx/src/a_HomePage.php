932e5456f59b6898e1a37998ef9ba3c57c3536ce
billy-harness <87822493+billy-harness@users.noreply.github.com>
2021-12-20 11:42:06 -0500

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://media.orrick.com/Media%20Library/public/images/client%20logos/sourcegraph-logo-740x360.jpg"; // Image (only available with version 2)
 $auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
