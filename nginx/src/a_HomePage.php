5d5b41ab96c969818db1d9c55b5f53459e7a5434
Konr Ness <konr.ness@harness.io>
2022-01-19 12:12:55 -0600

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -4,7 +4,7 @@
 ///////////////////////
 
 $auth_version=2; // app Version (1 or 2)
-$auth_icon="https://1000logos.net/wp-content/uploads/2017/03/McDonalds-logo.png"; // Image (only available with version 2)
-$auth_message="McDonalds"; // Text in the Landing page
+$auth_icon=""; // Image (only available with version 2)
+$auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
