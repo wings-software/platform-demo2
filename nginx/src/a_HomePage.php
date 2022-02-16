0faa8e97c58c7e097f85627dd3e127fe81e18436
Peter Hack <peterhack+github@gmail.com>
2022-02-16 06:32:00 -0800

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Tata_Consultancy_Services_Logo.svg/1200px-Tata_Consultancy_Services_Logo.svg.png"; // Image (only available with version 2)
+$auth_message="Hello TCS"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
