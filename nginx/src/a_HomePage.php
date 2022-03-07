25ef114f997e8979ced00f4d21e67b50af00836f
billy-harness <87822493+billy-harness@users.noreply.github.com>
2022-03-07 11:46:26 -0500

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://pbs.twimg.com/profile_images/1422233046925856768/p9Zr5nu6_400x400.png"; // Image (only available with version 2)
+$auth_message="Hey SecondFront Team!"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
