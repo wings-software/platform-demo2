03d51268313c38a54bf50e058fb53aee757b43e2
MarcoCelon <mcelon@gmail.com>
2022-03-08 10:34:19 +1100

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -1,7 +1,7 @@
 <?php
 /////////////////////////
 // --- HOME PAGE ---  //
-///////////////////////
+////////////////////////
 
 $auth_version=1; // app Version (1 or 2)
 $auth_icon=""; // Image (only available with version 2)
