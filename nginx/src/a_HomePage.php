7fbeb5c791388eb959ebb43357554f31985c28dd
billy-harness <87822493+billy-harness@users.noreply.github.com>
2022-03-08 14:44:11 -0500

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://i.pinimg.com/originals/d2/1a/ec/d21aec06ee2d1383f5635269c58df730.jpg"; // Image (only available with version 2)
 $auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
