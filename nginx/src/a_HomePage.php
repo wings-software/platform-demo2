87f0f8dfa557fc27a00aeb3bbd35573d26f54dae
Luis Redda <luis.redda@harness.io>
2022-03-17 08:30:52 -0300

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://upload.wikimedia.org/wikipedia/commons/9/91/Logo_Oficial_Banco_Original_-_Verde.png"; // Image (only available with version 2)
+$auth_message="Ola time"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
