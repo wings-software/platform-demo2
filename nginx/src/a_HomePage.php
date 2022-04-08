9157d45ebd9934fdaa59cd15a701466ec0cc1c90
Etienne Cointet <etienne@cointet.com>
2022-04-08 11:53:40 +0200

    BNP update

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://logos-world.net/wp-content/uploads/2021/02/BNP-Paribas-Logo-2000-2007.png"; // Image (only available with version 2)
+$auth_message="Welcome BNP! It's Friday :)"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
