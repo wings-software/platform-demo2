6a85010797551e54cc6ecbe32d4202691b277e44
billy-harness <87822493+billy-harness@users.noreply.github.com>
2022-05-04 13:01:07 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Seal_of_the_U.S._Department_of_Veterans_Affairs.svg/1200px-Seal_of_the_U.S._Department_of_Veterans_Affairs.svg.png"; // Image (only available with version 2)
+$auth_message="Testing a New Auth Icon"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
