980e0e60e043d3bb0dce76ceb8a9b94b7330aa8a
billy-harness <87822493+billy-harness@users.noreply.github.com>
2022-05-11 16:03:01 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://contractingacademy.gatech.edu/wp-content/uploads/2020/05/Air-Force-Seal.jpg"; // Image (only available with version 2)
+$auth_message="Testing a new Auth Icon"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
