021612948b281dbc9defd411eaf46ab7667a849f
Francis <105417373+francislabs@users.noreply.github.com>
2022-05-13 13:51:07 +0530

    Testing Home Page

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
-$internal_usage=""; // Put here anything you want
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://www.pngfind.com/pngs/m/60-600349_png-tweety-bird-tweety-bird-png-transparent-png.png"; // Image (only available with version 2)
+$auth_message="Hello World!"; // Text in the Landing page
+$internal_usage="Testing Application"; // Put here anything you want
 ?>
