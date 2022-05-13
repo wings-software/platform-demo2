8205e88f03fd0c9078bc4cb1dfa9eeaae6342237
Francis <105417373+francislabs@users.noreply.github.com>
2022-05-13 10:53:17 +0530

    Updated the page and testing new changes
    Updated the page and testing new changes

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
-$internal_usage=""; // Put here anything you want
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://www.pngfind.com/pngs/m/60-600349_png-tweety-bird-tweety-bird-png-transparent-png.png"; // Image (only available with version 2)
+$auth_message="New Version Demo"; // Text in the Landing page
+$internal_usage="Testing the Web app with updates"; // Put here anything you want
 ?>
