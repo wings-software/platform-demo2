96206b4389b5d949bcba8fb2f559b35209b4a9c8
Francis <105417373+francislabs@users.noreply.github.com>
2022-05-17 08:51:19 +0530

    Update home page fmlabs
    Testing update and deploy

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -4,7 +4,7 @@
 ///////////////////////
 
 $auth_version=2; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message="hello friday"; // Text in the Landing page
-$internal_usage=""; // Put here anything you want
+$auth_icon="https://www.pngfind.com/pngs/m/60-600349_png-tweety-bird-tweety-bird-png-transparent-png.png"; // Image (only available with version 2)
+$auth_message="hello Tuesday"; // Text in the Landing page
+$internal_usage="Testing it again"; // Put here anything you want
 ?>
