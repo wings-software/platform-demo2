724f68b4d653b61e9eff606be38d32f6a5dedac5
Etienne Cointet <etienne@cointet.com>
2022-05-31 14:07:23 +0200

    Lombard Odier

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://developer-lux.xs2a.lombardodier.com/system/files/inline-images/Lombard_Odier_logo.svg_.png"; // Image (only available with version 2)
 $auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
