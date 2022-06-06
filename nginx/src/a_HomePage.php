4259d0d1aff5552cd7d1779cf19e09349c9c73de
billy-harness <87822493+billy-harness@users.noreply.github.com>
2022-06-06 09:43:42 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://norsemanservices.com/wp-content/uploads/2019/12/NDS-Logo-Mobile-Header.png"; // Image (only available with version 2)
+$auth_message="Testing New Auth Icon"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
