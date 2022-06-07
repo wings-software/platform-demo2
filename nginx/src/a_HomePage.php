5662738a7febda5d5a949c352b928a4f6f4161d6
Etienne Cointet <etienne@cointet.com>
2022-06-07 13:43:32 +0200

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://cms.harness.io/uploads/Canary_Top_Gun_6eb319490f.png"; // Image (only available with version 2)
+$auth_message="AMADEUS"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
