48821f6eb3bbc4a9082a3f64abdbea1ab293beed
billy-harness <87822493+billy-harness@users.noreply.github.com>
2022-06-21 09:32:08 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://1blm3kqgpnx34451110jn6vd-wpengine.netdna-ssl.com/wp-content/uploads/2021/03/Forge.png"; // Image (only available with version 2)
+$auth_message="Good Morning Forge Team!"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
