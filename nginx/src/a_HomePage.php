ba6d3a67ebc3294cab5dd770e0b796479848acc0
Gabriel (Gabs) Cerioni <80788917+gabrielcerioni@users.noreply.github.com>
2022-07-22 15:20:58 -0300

    Testing Demo for a training session w/ Partners

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -4,7 +4,7 @@
 ///////////////////////
 
 $auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
+$auth_icon="https://www.azpetshop.com.br/blog/wp-content/uploads/2020/11/shiba-inu-guia-racas-805x452.jpg"; // Image (only available with version 2)
 $auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
