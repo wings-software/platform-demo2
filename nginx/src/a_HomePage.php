f73d6733a5ca57e5626ef44765d1933120c67e26
Luis Redda <luis.redda@gmail.com>
2022-07-28 11:08:57 -0300

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -3,8 +3,8 @@
 // --- HOME PAGE ---  //
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
-$auth_icon=""; // Image (only available with version 2)
-$auth_message=""; // Text in the Landing page
+$auth_version=2; // app Version (1 or 2)
+$auth_icon="https://harness.io/share_card.jpg"; // Image (only available with version 2)
+$auth_message="DevopsDays Medellin"; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
 ?>
